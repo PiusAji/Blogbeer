01631d96244a9948cfe030e3f89b6dbeacfe1bef
Pius Aji <piusaji995@gmail.com>
2025-07-21 14:25:17 +0800

    Cloudinary domains added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import { withPayload } from "@payloadcms/next/withPayload";
 
 const nextConfig = {
   /* config options here */
+  images: {
+    domains: ["res.cloudinary.com"], // Add this line
+    // ... your other config
+  },
 };
 
 export default withPayload(nextConfig);
